737cb902677e961758e47ab378dfff8b2c64124b
AConcrnedCitizen <80365234+AConcrnedCitizen@users.noreply.github.com>
2022-05-30 15:32:55 +1000

    a

---
+++ humidity.php
@@ -0,0 +1,24 @@
+<?php
+
+$conn -> select_db("testsite");
+$sql = "SELECT * FROM humidity";
+if($result = mysqli_query($conn, $sql)){
+    if(mysqli_num_rows($result) > 0){
+        while($row = mysqli_fetch_array($result)){
+            echo "<tr>";
+                $mintemp = "<td>" . $row[(date("d"))-1] . "</td>";
+            echo "</tr>";
+        }
+        echo "</table>";
+        // Free result set
+        mysqli_free_result($result);
+    } else{
+        echo "No records matching your query were found.";
+    }
+
+} else{
+    echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
+}
+
+
+?>
--- index.php
+++ index.php
@@ -1,51 +1,44 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="utf-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <title>BOMB</title>
-        <meta name="description" content="">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="main.css">
-        <link rel="stylesheet" href="grid.css">
-        <link rel="preconnect" href="https://fonts.googleapis.com">
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet"> 
-    </head>
-    <body>
-        
-    <?php
-        require_once("config.php");
-        
-        $conn -> select_db("testsite");
-        $sql = "SELECT * FROM temp";
-        if($result = mysqli_query($conn, $sql)){
-            if(mysqli_num_rows($result) > 0){
-                while($row = mysqli_fetch_array($result)){
-                    echo "<tr>";
-                        $temperature = "<td>" . $row[(date("d"))-1] . "</td>";
-                    echo "</tr>";
-                }
-                echo "</table>";
-                // Free result set
-                mysqli_free_result($result);
-            } else{
-                echo "No records matching your query were found.";
-            }
-        } else{
-            echo "ERROR: Could not able to execute $sql. " . mysqli_error($conn);
-        }
 
-        ?>
-        
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>BOMB</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="main.css">
+    <link rel="stylesheet" href="grid.css">
+
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@500&display=swap" rel="stylesheet">
+</head>
+
+<body>
+
+    <?php
+    require_once("config.php");
+    require_once("temp.php");
+    require_once("humidity.php");
+    require_once("mintemp.php");
+    require_once("maxtemp.php");
+    ?>
+    <div class="page">
         <div class="grid-container">
             <div class="item1"><img src="./img/BOMB.png" style="width: 45%;"></div>
-            <div class="item2"><?=$temperature?></div>
-            <div class="item3">MinMax</div>  
+            <div class="item2"><?= $temperature ?>°</div>
+            <div class="item3"><?= $mintemp ?>° | <?= $maxtemp ?>°</div>
             <div class="item4">Humidity</div>
-            <div class="item5">Forecast</div>
-          </div>
-        
+            <div class="item5">Tomorrow</div>
+            <div class="item6">Day2</div>
+            <div class="item7">Day3</div>
+            <div class="item8">Day4</div>
+            <div class="item9">Day5</div>
         </div>
-    </body>
+
+    </div>
+    </div>
+</body>
+
 </html>
